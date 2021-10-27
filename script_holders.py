6fbabb04b13d1c74da2872bec5d9943296921a56
Hamza Ahmed <45544623+Hamza-101@users.noreply.github.com>
2021-10-28 01:44:12 +0500

    Update script_holders.py

--- script_holders.py
+++ script_holders.py
@@ -1,14 +1,18 @@
 
-rom selenium import webdriver
+from datetime import datetime
+from functools import total_ordering
+from pandas.core.base import DataError
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import csv
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import string
-from csv import reader,writer
+from csv import reader
 import code
 import pandas
+from datetime import datetime
 
 from selenium.webdriver.support.wait import WebDriverWait
 
@@ -18,61 +22,68 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 
 
-ga=pandas.read_csv("adresses.csv")
 
-for i in range (ga.size-1):
-        
-        fj=ga.iloc[i+10][0]
-        print(fj)
-        print(i)
-        driver = webdriver.Firefox()
-        url="https://etherscan.io/token/"+str(fj)+"#readContract"
-
-
-#driver = webdriver.Chrome()
-
-        driver.get(url)
-
-        time.sleep(5)  # JavaScript needs time to add elements on page
-
-        frame = driver.find_element_by_id('readcontractiframe')
-        driver.switch_to.frame(frame)
-
-        driver.find_element_by_xpath('//a[text()="[Expand all]"]').click()
-        time.sleep(0.5)  # JavaScript needs time to expand all
-
-        sik=driver.find_elements_by_css_selector("div.card.shadow-none.mb-3")
-        
-        driver1=webdriver.Firefox()
-        driver1.get("https://etherscan.io")
-        wait = WebDriverWait(driver1,5)
-        key=wait.until(EC.element_to_be_clickable((By.XPATH,"/html/body/div[1]/main/div/div[3]/div[1]/div/div[2]/div[1]/div/div[1]/div[1]/div/div[2]/a")))
-        val=key.text
+ga=pandas.read_csv("adres.csv")
+d=[]
+file= open('script_holders.csv', 'a')
+for i in range((ga.size-1)):
+    driver=webdriver.Firefox()
+    fj=ga.iloc[i][0]
+    print("jfnd")
+    print(i)
+    print("jfnd")
+    wait1 = WebDriverWait(driver, 5)
+    driver.get("https://etherscan.io/token/0x3146f5df6b081bda369b61d85f33bb517e62da53#balances")
+    wait = WebDriverWait(driver, 20)
+    wait.until(EC.frame_to_be_available_and_switch_to_it((By.ID, "tokeholdersiframe")))
+    get = wait1.until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div[3]/table/tbody/tr[1]/td/div"))).text
+    timen = datetime.now()
+    dt_string= timen.strftime("%d/%m/%Y %H:%M:%S")
+    print(dt_string)
+    if (str(get)!="There are no matching entries"):
+        # d.append(str(fj))
+        # d.append("")
+        num = wait.until(EC.visibility_of_element_located((By.XPATH, "(//span[contains(@class,'page-link text-nowrap')]/strong)[2]")))
+        val=int(num.get_attribute('innerText'))
         print(val)
-        driver1.close()
-
-        fd=[]
-        c=[]
-
-        num=len(sik)
         print(i)
-        for i in range(1,(num+1)):
-            print('---', i, '---')
-            fd.append(driver.find_element_by_id(f"readHeading{i}").text)
-            c.append(driver.find_element_by_id(f"readCollapse{i}").text)
-            
-
-        print(fd)
-        print(c)
-        
-        file= open('script_public_variables.csv', 'a')
-
-
+        print("sdjkbfkjsd") 
         writer = csv.writer(file)
-        writer.writerow(fj) 
-        writer.writerow(val)
-        writer.writerow(fd) 
-        writer.writerow(c) 
-        
-        file.close()
-        driver.close()
+        writer.writerow(fj)
+        writer.writerow(dt_string)
+
+        for i in range(val):  
+                        num = wait.until(EC.visibility_of_element_located((By.XPATH, "(//span[contains(@class,'page-link text-nowrap')]/strong)[1]")))
+                        value=int(num.get_attribute('innerText'))
+                        print(value)
+                        print(i)    
+                        print("udj")
+                        # simpleTable = driver.find_element(By.XPATH,"/html/body/div[2]/div[3]/table")
+                        rows = driver.find_elements(By.TAG_NAME,"tr")
+                        for i in range(1,len(rows)):
+                                cols = rows[i].find_elements(By.TAG_NAME,"td")
+                                for g in cols:
+                                        d.append(g.text)
+                                        
+                        
+                        
+                        print(d)
+                        if((val!=value)):
+                            next = wait1.until(EC.element_to_be_clickable((By.XPATH,"//div[@class='d-inline-block']//a[@aria-label='Next']")))
+                            driver.execute_script("arguments[0].scrollIntoView(true);",next)
+                            driver.execute_script("window.scrollBy(0,-200);")
+                            next.click() 
+                        else:
+                            writer.writerow(d)
+                            
+                            writer.writerow("\n")
+                            break
+                              
+    driver.close()
+    break
+   
+
+    
+    
+
+file.close()
